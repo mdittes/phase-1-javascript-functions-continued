a8cd8fd915822a546f6eb624e2292db064591114
Matt Dittes <mattdittes@gmail.com>
2022-07-19 11:35:18 -0400

    commit tree 31bb7193de63415b97226f4384fe78877559932c

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(something = 'go to the office'){
+    return `This Monday, I will ${something}.`
+}
+
+function wrapAdjective(flair = "*"){
+    return function(adj = 'special'){
+        return `You are ${flair}${adj}${flair}!`
+    }
+}
